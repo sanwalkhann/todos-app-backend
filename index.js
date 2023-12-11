9ca7b37fbbcb05c6846e324ba464f79e472b00cf
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-11 23:42:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
-
-
-app.use('/api', routes);
 mongoose.connect(process.env.MONGO_URL)
  mongoose.connection.once("connected",()=>{
     console.log("Database Connected")
  })
+
+
+app.use('/api', routes);
 
 
  app.listen(process.env.PORT,()=>{
