6732b3ff3c6dc8f37a9f69eea6f00d884155ec79
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-12 11:54:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const routes = require('./routes/routes.js');
 
 dotenv.config();
 
-const app = express();
 
-const corsOptions = {
-   origin: 'http://localhost:3000', 
-   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-   credentials: true, 
- };
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
  
- app.use(cors(corsOptions));
+ app.use(cors());
 
 app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URL);
