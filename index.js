40172c8ebbc99819eabf2b330af86f86eb7462a7
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-12 11:01:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const routes = require('./routes/routes.js'); 
-const cors = require('cors')
-
+const cors = require('cors'); 
+const routes = require('./routes/routes.js');
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors()); 
 
-app.use(cors())
 app.use(bodyParser.json());
-mongoose.connect(process.env.MONGO_URL)
- mongoose.connection.once("connected",()=>{
-    console.log("Database Connected")
- })
-
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.once('connected', () => {
+  console.log('Database Connected');
+});
 
 app.use('/api', routes);
 
-
- app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on: ${process.env.PORT}`)
- })
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on: ${process.env.PORT}`);
+});
